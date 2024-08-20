812049f17256d6f39bca46b5298ad1531c5d2148
yeanha <143509984+yeanha@users.noreply.github.com>
2024-08-20 20:02:27 +0900

    cpp

---
+++ Pyramid_Story/Source/Pyramid_Story/Pyramid_Story.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Pyramid_Story.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pyramid_Story, "Pyramid_Story" );
